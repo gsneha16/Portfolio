d1fea6316643b3726e205318d2e79031b30a3533
gsneha16 <gsneha2319@gmail.com>
2025-05-31 14:03:43 +0530

    responsiveness

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import {NavLink} from 'react-router-dom';
 import './main.css';
 
@@ -7,6 +8,8 @@ const navbar = () => {
   return (
    <div className='navbar'>
     <div className="flex-btwn navbar-container">
+        {/* <div className="hamberger"><GiHamburgerMenu />
+</div> */}
         <h2 className="logo">Sneha Goyal</h2>
         <ul className="flex-btwn nav-list">
             <li>Services</li>
